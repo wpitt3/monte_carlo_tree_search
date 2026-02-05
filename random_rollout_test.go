ba825823c15eba41099aa9fa11eaf1a1ed7f643a
Will Pitt <wpitt3@gmail.com>
2026-02-05 18:31:41 +0000

    fix: fixing rollout policy

--- random_rollout_test.go
+++ random_rollout_test.go
@@ -8,12 +8,17 @@ func (_ RandomRollout) Rollout(originalBoard State[Action]) float32 {
 	board := originalBoard.Copy()
 	result := board.Winner()
 	done := result != 0 || board.IsEndState()
+	movesCount := 0
 	for !done {
 		moves := board.ValidActions()
 		move := moves[rand.Intn(len(moves))]
 		board = board.PerformMove(move)
 		result = board.Winner()
 		done = result != 0 || board.IsEndState()
+		movesCount++
+	}
+	if movesCount%2 == 1 {
+		result *= -1
 	}
 	if result == 0 {
 		return 0.5
